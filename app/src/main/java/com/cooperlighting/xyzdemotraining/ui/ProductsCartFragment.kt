c1f310fd3ef57455d214440da0991bcb6d352924
pmessina <pmm0501@gmail.com>
2022-01-26 21:13:27 -0500

    Theme updates; refactoring

--- app/src/main/java/com/cooperlighting/xyzdemotraining/ui/ProductsCartFragment.kt
+++ app/src/main/java/com/cooperlighting/xyzdemotraining/ui/ProductsCartFragment.kt
@@ -11,7 +11,7 @@ import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.cooperlighting.xyzdemotraining.R
 import com.cooperlighting.xyzdemotraining.ProductsViewModel
-import com.cooperlighting.xyzdemotraining.contactsviewer.ShoppingCartRecyclerViewAdapter
+import com.cooperlighting.xyzdemotraining.ShoppingCartRecyclerViewAdapter
 import com.cooperlighting.xyzdemotraining.databinding.FragmentProductsCartBinding
 import org.koin.android.viewmodel.ext.android.viewModel
 
